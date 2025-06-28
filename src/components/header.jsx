9a28cbf0bd2b250ef94b5d6c94ea25ccd6fcf419
Muhammad Junaid <121503655+junaidsalam639@users.noreply.github.com>
2025-06-28 21:07:09 +0500

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -89,6 +89,7 @@ export default function Header() {
                   {link.name}
                 </Link>
               ))}
+               <LanguageSelector />
             </nav>
           </div>
         </div>
